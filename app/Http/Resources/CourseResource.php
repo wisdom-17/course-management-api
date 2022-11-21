bc526ffd21110fe601ed635e8782121d5eadd8c9
Saeed <saeed17@gmail.com>
2022-11-21 11:52:29 +0000

    Return date in original format

--- app/Http/Resources/CourseResource.php
+++ app/Http/Resources/CourseResource.php
@@ -17,12 +17,12 @@ class CourseResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'startDate' => date('d/m/Y', strtotime($this->start_date)),
-            'endDate' => date('d/m/Y', strtotime($this->end_date)),
+            'startDate' => $this->start_date,
+            'endDate' => $this->end_date,
             'teachingDays' => $this->teaching_days,
-            'createdAt' => date('d/m/Y', strtotime($this->created_at)),
-            'updatedAt' => date('d/m/Y', strtotime($this->updated_at)),
-            'deletedAt' => date('d/m/Y', strtotime($this->start_date)),
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+            'deletedAt' => $this->start_date,
         ];
     }
 }
