414fbff2b8bb1da777c6c837af8c7e945e86e747
Saeed <saeed17@gmail.com>
2022-11-28 18:10:42 +0000

    Teacher Model, Controller, Request classes and route definitions

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTeacherRequest;
+use App\Http\Requests\UpdateTeacherRequest;
+use App\Models\Teacher;
+
+class TeacherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTeacherRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTeacherRequest $request)
+    {
+        $teacher = new Teacher();
+        $teacher->name = $request->name;
+        $teacher->hourly_rate = $request->hourly_rate;
+        $teacher->save();
+
+        return response()->json([
+            'message' => 'Saved new Teacher successfully',
+            'id' => $teacher->id
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Teacher  $teacher
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Teacher $teacher)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTeacherRequest  $request
+     * @param  \App\Models\Teacher  $teacher
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTeacherRequest $request, Teacher $teacher)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Teacher  $teacher
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Teacher $teacher)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\TokenController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\CourseDatesController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -34,13 +35,16 @@ Route::middleware(['auth:sanctum'])->group(function () {
     // - defined outside of apiResources because we want to
     // delete multiple courses using this endpoint
     // 
-    // - defined as a PATCH request because we are soft deleting (this is not considered DELETE)
+    // - defined as a PATCH request because we are soft deleting (according to REST this is not DELETE )
     // soft deleting just changes the value of the deleted_at column
     Route::patch('/courses', [CourseController::class, 'destroy']);
+    Route::patch('/teachers', [TeacherController::class, 'destroy']);
 
     // we don't want the default destroy route, as it supports deleting one record 
     // we want to delete multiple in one request hence the above
     Route::apiResource('courses', CourseController::class)->except('destroy');
+
+    Route::apiResource('teachers', TeacherController::class)->except('destroy');
 
     Route::apiResources([
         // 'courses' => CourseController::class,
