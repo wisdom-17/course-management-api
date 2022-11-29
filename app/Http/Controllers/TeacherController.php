79b3e8b8ece89954176c212d9a7997898b459704
Saeed <saeed17@gmail.com>
2022-11-29 22:28:15 +0000

    List all teachers

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTeacherRequest;
 use App\Http\Requests\UpdateTeacherRequest;
+use App\Http\Resources\TeacherResource;
 use App\Models\Teacher;
 
 class TeacherController extends Controller
@@ -15,7 +16,9 @@ class TeacherController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(
+            TeacherResource::collection(Teacher::all())
+        , 200);
     }
 
     /**
---
+++ app/Http/Resources/TeacherResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TeacherResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return[            
+            'id' => $this->id,
+            'name' => $this->name,
+            'hourlyRate' => $this->hourly_rate,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+            'deletedAt' => $this->deleted_at
+        ];
+    }
+}
