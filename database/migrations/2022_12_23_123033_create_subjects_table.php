feb45216d2e813f5928fe44d0593343020dffbf4
Saeed <saeed17@gmail.com>
2022-12-25 10:07:38 +0000

    Subjects table migration

---
+++ database/migrations/2022_12_23_123033_create_subjects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->foreignId('teacher_id')->constrained('teachers');
+            $table->foreignId('course_calander_id')->constrained('course_calendars');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
