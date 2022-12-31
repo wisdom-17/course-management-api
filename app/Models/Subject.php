cccabbb8fb1fa80281c701ebfeefc9e54a16f17d
Saeed <saeed17@gmail.com>
2022-12-31 19:15:16 +0000

    hasMany relationship between subject and subject days times

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model
 {
     use HasFactory;
+
+    public function subjectDaysTimes()
+    {
+        return $this->hasMany(SubjectDayTime::class);
+    }
 }
