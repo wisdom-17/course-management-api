d2c64d9001209cc8c61b5ad90629d1e5cf1b345b
Saeed <saeed17@gmail.com>
2022-12-31 17:53:27 +0000

    Store teacher test. Improvements to remaining tests.

--- tests/Feature/TeacherTest.php
+++ tests/Feature/TeacherTest.php
@@ -9,7 +9,7 @@ beforeEach(function () {
 
 });
 
-test('returns an array of teacher objects', function () {
+test('list teachers returns an array of teacher objects', function () {
     $response = $this->actingAs($this->user)
         ->get('/api/teachers')
         ->assertJson(fn (AssertableJson $json) => 
@@ -21,5 +21,12 @@ test('returns an array of teacher objects', function () {
 test('empty teacher data throws validation error when saving', function () {
     $response = $this->actingAs($this->user)
         ->postJson('/api/teachers')
-        ->assertStatus(422);
+        ->assertInvalid(['name', 'hourlyRate']);
+});
+
+test('teacher saves successfully to db when valid teacher data provided', function () {
+    $response = $this->actingAs($this->user)
+        ->postJson('/api/teachers', ['name' => 'Test Teacher 1', 'hourlyRate' => 25.00])
+        ->assertValid()
+        ->assertCreated();
 });
