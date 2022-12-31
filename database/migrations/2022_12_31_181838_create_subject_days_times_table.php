e094739b51697a6f7f2cf1162cfb222545967fa6
Saeed <saeed17@gmail.com>
2022-12-31 19:14:43 +0000

    Model, factory and migration files for Subject Days Times

---
+++ database/migrations/2022_12_31_181838_create_subject_days_times_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subject_days_times', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subject_id')->constrained('subjects');
+            $table->string('day', 255);
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject_days_times');
+    }
+};
