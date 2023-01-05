f5a82a84ddf8c151377fca66a88db71af32a1f2c
Saeed <saeed17@gmail.com>
2023-01-05 22:01:16 +0000

    Test for list subjects endpoint

--- tests/Feature/SubjectsTest.php
+++ tests/Feature/SubjectsTest.php
@@ -1,9 +1,22 @@
 <?php
+
+use App\Models\Subject;
 use App\Models\User;
+use Illuminate\Testing\Fluent\AssertableJson;
 
 beforeEach(function () {
     $this->user = User::factory()->create();
+    $this->subjects = Subject::factory()->count(3)->create();
+});
 
+test('list subjects returns an array of subjects objects', function () {
+    $response = $this->actingAs($this->user)
+        ->get('/api/subjects')
+        ->assertJson(fn (AssertableJson $json) => 
+            $json->has(3)
+        )
+        ->assertStatus(200);
+        // TODO: test for expected properties
 });
 
 test('empty subject data throws validation error when saving', function () {
