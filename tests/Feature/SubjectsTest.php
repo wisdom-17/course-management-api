5be5d55e9898517fca38fb6eaa1b640f0e79cd57
Saeed <saeed17@gmail.com>
2023-01-06 08:05:02 +0000

    Test for teachers in list subjects endpoint

--- tests/Feature/SubjectsTest.php
+++ tests/Feature/SubjectsTest.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Models\Subject;
+use App\Models\Teacher;
 use App\Models\User;
 use Illuminate\Testing\Fluent\AssertableJson;
 
 beforeEach(function () {
     $this->user = User::factory()->create();
-    $this->subjects = Subject::factory()->count(3)->create();
+    $this->subjects = Subject::factory()
+        ->has(Teacher::factory()->count(1))
+        ->count(3)->create();
 });
 
 test('list subjects returns an array of subjects objects', function () {
@@ -14,7 +17,10 @@ test('list subjects returns an array of subjects objects', function () {
         ->get('/api/subjects')
         ->assertJson(fn (AssertableJson $json) => 
             $json->has('subjects', 3, fn ($json) =>
-                $json->hasAll(['id', 'name', 'createdAt', 'updatedAt', 'deletedAt'])
+                $json->hasAll(['id', 'name', 'teachers', 'createdAt', 'updatedAt', 'deletedAt'])
+                    ->has('teachers', 1, fn ($teacherJson) => 
+                        $teacherJson->hasAll(['id', 'name', 'hourlyRate', 'createdAt', 'updatedAt', 'deletedAt'])
+                    )
             )            
         )
         ->assertStatus(200);
