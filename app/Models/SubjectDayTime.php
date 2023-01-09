583ba7c0ac656338d4585feeaf2a742461eed941
Saeed <saeed17@gmail.com>
2023-01-09 08:26:34 +0000

    Subject relation

--- app/Models/SubjectDayTime.php
+++ app/Models/SubjectDayTime.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class SubjectDayTime extends Model
 {
     use HasFactory;
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
 }
