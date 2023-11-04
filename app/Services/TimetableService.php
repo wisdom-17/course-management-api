7f2d672a2ef0107e2ab9ac0652504a0fe2fb2eed
Saeed <saeed17@gmail.com>
2023-11-04 22:23:03 +0000

    Implement initial methods to calculate the days of the week the course is taught on

--- app/Services/TimetableService.php
+++ app/Services/TimetableService.php
@@ -2,18 +2,33 @@
 
 namespace App\Services;
 
-use App\Models\Course;
-
 class TimetableService
 {
-    public function generateTimetable($courseId)
-    {
-        $days = [];
-        // Get the course
-        $course = Course::find($courseId);
+    private $course;
 
-        // loop through each subject and get the day
-        foreach ($course->subjects as $subject) {
-            // 
+    public function generateTimetable($course)
+    {
+        if ($course) {
+            $this->course = $course;
+        } else {
+            throw new \Exception('Invalid course for timetable generation');
+        }
+
+        // 1) Get the days the subject(s) are taught on
+        $days = $this->getDays();
+
+        $timetable = array_fill_keys($days, []);
+        return $timetable;
+    }
+
+    private function getDays()
+    {
+        $days = $this->course->subjects->map(function ($subject) {
+            return $subject->subjectDayTimes->map(function ($dayTime) {
+                return $dayTime->day;
+            });
+        })->flatten()->unique()->toArray();
+
+        return $days;
     }
 }
